08ffd99022b001f237f7b3aad62272c45cd34815
Alvaro <varito1@gmail.com>
2020-06-10 00:56:43 -0300

    Lesson 53

---
+++ app/src/main/java/alvus/com/swoosh/Constants.kt
@@ -0,0 +1,3 @@
+package alvus.com.swoosh
+
+const val EXTRA_LEAGUE = "league"
---
+++ app/src/main/java/alvus/com/swoosh/SkillActivity.kt
@@ -0,0 +1,15 @@
+package alvus.com.swoosh
+
+import android.os.Bundle
+
+class SkillActivity : BaseActivity() {
+
+    var league = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+        league = intent.getStringExtra(EXTRA_LEAGUE)
+        println(league)
+    }
+}
